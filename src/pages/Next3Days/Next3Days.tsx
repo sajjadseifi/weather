92dbc90d13f4e060a8432d541d34258569e3bffe
Sajjad Seifi <s.09334497255@gmail.com>
2023-03-28 19:01:51 -0400

    feature : use dummy data & impliment selected item

--- src/pages/Next3Days/Next3Days.tsx
+++ src/pages/Next3Days/Next3Days.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import Card from "../../components/card/Card";
-import { CardType } from "../../components/card/card-type";
+import React, { useState } from "react";
 import CardGrid from "../../components/card/CardGrid";
+import { WheatherStatus } from "../../constants/enum/wheather-status.enum";
+import OtherDay from "./components/OtherDay";
+import SelectedDay from "./components/SelectedDay";
+import { next3DayDummyData } from "./dummy-data";
 
-const Next3Days = ()=>{
+const Next3Days = ()=> {
+    const [selected,setSelected]  = useState(0);
+
+    const onSelectClick = (index:number) => {
+        setSelected(prv => {
+            if (index < 0) return prv;
+            if (index >= next3DayDummyData.length) return prv;
+
+            return index
+        })
+    }
+
     return (
         <div>
             <CardGrid>
-                <Card cardType={CardType.SKY}>1</Card>
-                <Card cardType={CardType.DARK}>2</Card>
-                <Card cardType={CardType.DARK}>3</Card>
-                <Card cardType={CardType.DARK}>3</Card>
+                {next3DayDummyData.map((props,index)=> {
+                    if (selected == index) {
+                        return  <SelectedDay  {...props} />
+                    }
+                    else {
+                        return  <OtherDay onClick={() => onSelectClick(index)} {...props} />   
+                    }
+                })}
             </CardGrid>
         </div>
     )
