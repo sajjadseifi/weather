6d4782d6cef7f128bc08145c86dda7231a18b01c
Sajjad Seifi <s.09334497255@gmail.com>
2023-03-28 13:34:54 -0400

    test : use card & card grid

--- src/pages/Next3Days.tsx
+++ src/pages/Next3Days.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import Card from "../components/card/Card";
+import { CardType } from "../components/card/card-type";
+import CardGrid from "../components/card/CardGrid";
 
 const Next3Days = ()=>{
     return (
-        <div>Next 3 Days</div>
+        <div>
+            <CardGrid>
+                <Card cardType={CardType.SKY}>1</Card>
+                <Card cardType={CardType.DARK}>2</Card>
+                <Card cardType={CardType.DARK}>3</Card>
+                <Card cardType={CardType.DARK}>3</Card>
+            </CardGrid>
+        </div>
     )
 }
 
