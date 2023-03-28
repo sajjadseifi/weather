0fbf615eeecff2436266cb9db3e2d12309489b6e
Sajjad Seifi <s.09334497255@gmail.com>
2023-03-28 18:53:20 -0400

    fixed : props interface type

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,18 +2,17 @@ import React, { FC } from "react";
 import styles from './Card.module.css'
 import { CardType } from "./card-type";
 
-interface CardProps {
+interface CardProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    cardType:CardType
     style?: any
     children? : any
-    cardType:CardType
 }
 
 const Card : FC<CardProps> = (props) => { 
     const classType = styles[props.cardType]
     const cardClass = [styles.Card,classType].join(' ')
-    console.log(props.cardType,cardClass)
     return (
-        <div className={cardClass} style={props.style}>
+        <div className={cardClass} style={props.style} {...(props as any)}>
             {props.children}
         </div>
     )
