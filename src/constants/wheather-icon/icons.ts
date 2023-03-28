af30090609562ded547552885b82464c2c409405
Sajjad Seifi <s.09334497255@gmail.com>
2023-03-28 18:57:43 -0400

    export icons name with object

---
+++ src/constants/wheather-icon/icons.ts
@@ -0,0 +1,18 @@
+import SunnyIcon from '../../assets/icon/wheathers/sunny.png'
+import CloudIcon from '../../assets/icon/wheathers/cloud.png'
+import NightIcon from '../../assets/icon/wheathers/night.png'
+import StromyIcon from '../../assets/icon/wheathers/stromy.png'
+import SleetyIcon from '../../assets/icon/wheathers/sleety.png'
+import RainyIcon from '../../assets/icon/wheathers/rainy.png'
+import RainyBadIcon from '../../assets/icon/wheathers/rainy-bad.png'
+
+
+export {
+    CloudIcon,
+    NightIcon,
+    RainyBadIcon,
+    RainyIcon,
+    StromyIcon,
+    SunnyIcon,
+    SleetyIcon
+}
