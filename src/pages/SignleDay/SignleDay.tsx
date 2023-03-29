8e4431dd43b878e31c2ee0be7dccb59486dae765
Sajjad Seifi <s.09334497255@gmail.com>
2023-03-29 18:17:49 -0400

    feature[route] : add single day route

--- src/pages/SignleDay/SignleDay.tsx
+++ src/pages/SignleDay/SignleDay.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from "react";
 import qs from 'qs'
-import {  useLocation } from "react-router-dom";
+import styles from './SingleDay.module.css'
+import WtheaderDeg from "../../components/ui/WtheaderDeg";
+import { choseWheatherIcon } from "../../constants/wheather-icon/chose-wheather";
+import { WheatherStatus } from "../../constants/enum/wheather-status.enum";
+import Card from "../../components/card/Card";
+import { CardType } from "../../components/card/card-type";
+import Forcast from "./components/Forcast/Forcast";
+import { forcastData } from "./components/Forcast/forcast-data-dummy";
+import CardTitle from "../../components/CardTitle/CardTitle";
+import AirCondition from "./components/AirCondition/AirCondition";
 
-
-type SignleDayType = 'TODAY' | 'TOMORROW'
+export type SignleDayType = 'TODAY' | 'TOMORROW'
 
 interface SignleDayProps {
     flag:SignleDayType
@@ -12,7 +20,32 @@ interface SignleDayProps {
 const SignleDay : FC<SignleDayProps> = (props) =>{
 
     return (
-        <div>{props.flag}</div>
+        <div className={styles.SignleDay}>
+            <div className={styles.Top}>
+                <div className={styles.DegAndCityWrapper}>
+                    <div className={styles.CityWrapper}>
+                        <h2 className={styles.City}>Madrid</h2>
+                        <span className={styles.Chance}>Chance of rain : 0%</span>
+                    </div>
+                    <div className={styles.DegWrapper}>
+                        <WtheaderDeg value={50} />
+                    </div>
+                </div>
+                <div className={styles.IconWraper}>
+                    <img src={choseWheatherIcon(WheatherStatus.Sunny,{})}/>
+                </div>
+            </div>
+            <div className={styles.Forcast}>
+                <Forcast flag={props.flag} forcasts={forcastData} />
+                <AirCondition
+                changeOfRain={0}
+                realFeel={30}
+                uv={3}
+                wind="0.2 km/h"
+                
+                />
+            </div>
+        </div>
     )
 }
 
