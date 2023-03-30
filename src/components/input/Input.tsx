4f37d2d3c3a7ca632537a6ff4bd04fca93eb3731
Sajjad Seifi <s.09334497255@gmail.com>
2023-03-30 06:03:24 -0400

    feature : add clickable input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,17 +1,28 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from './Input.module.css'
 
 interface InputProps  { 
     icon?:any
     input: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+    onClick?:(text:string) => any
 }
 
 const Input : FC<InputProps> = (props) => {
+    const [text,setText] = useState('')
+
     return (
         <div className={styles.Input}>
-            <div className={styles.Icon}>{props.icon}</div>
+            <div 
+            
+            className={styles.Icon}
+            onClick={()=> {
+                if (props.onClick){
+                    props.onClick(text)
+                }
+            }}
+            >{props.icon}</div>
             <div className={styles.TextField}>
-                <input type="text" {...props.input} />
+                <input onChange={({target:{value}})=>setText(value)} type="text" {...props.input} />
             </div>
         </div>
     )
